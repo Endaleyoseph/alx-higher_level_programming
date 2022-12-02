03036eae4c703e06f8e150e9a1125c1cc2fa0102
Yoseph Endale <endaleyoseph1@gmail.com>
2022-12-02 09:46:52 -0800

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/usr/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for y in x:
+            if y == x[len(x) - 1]:
+                print("{:d}".format(y), end="")
+            else:
+                print("{:d}".format(y), end=" ")
+        print()
