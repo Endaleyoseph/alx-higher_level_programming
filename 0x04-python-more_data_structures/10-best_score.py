d5689054ea3697d22df718dc0ed6d9d44b3d5ffc
Yoseph Endale <endaleyoseph1@gmail.com>
2022-12-07 04:30:34 -0800

    best score1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     else:
-        score = 0
-        for k, v in a_dictionary.items():
-            if v > score:
-                score = v
-                s = k
-        return s
+        return max(a_dictionary, key=a_dictionary.get)
