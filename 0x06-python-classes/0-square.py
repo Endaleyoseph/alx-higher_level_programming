b6ef1ccce27b43b0739c3fe9f34d9f20c52db648
Yoseph Endale <endaleyoseph1@gmail.com>
2022-12-20 03:36:26 -0800

    My first square fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-class Square:
-    """
-    Square is an empty class
-    """
+"""
 
+Module 0-square
+Defines class Square
+"""
+
+
+class Square:
+    """Empty class"""
     pass
