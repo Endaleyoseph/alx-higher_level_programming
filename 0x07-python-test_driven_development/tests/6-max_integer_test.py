addabcc152fdaddf5a721d5a79ca2f3d090e9726
Yoseph Endale <endaleyoseph1@gmail.com>
2022-12-29 07:36:50 -0800

    add tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,8 +8,31 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test case for max_integer funtion
     using test_max_integer method"""
+    def test_module_docstring(self):
+        moduleDoc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(moduleDoc) > 1)
+
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 5]), 5)
         self.assertEqual(max_integer([1, 2, 3, -5]), 3)
         self.assertEqual(max_integer([1, 2, 7, 3]), 7)
         self.assertEqual(max_integer([]), None)
+
+    def test_max_float(self):
+        self.assertEqual(max_integer([1, 2.9, 3, 5.5]), 5.5)
+        self.assertEqual(max_integer([{5.5, 1.1}, {4, 0}]), {5.5, 1.1})
+
+    def test_max_strings(self):
+        self.assertEqual(max_integer("123456"), "6")
+        self.assertEqual(max_integer(""), None)
+
+    def test_exceptions(self):
+        with self.assertRaises(TypeError):
+            max_integer(45)
+        with self.assertRaises(TypeError):
+            max_integer([-5, 7, "9"])
+        with self.assertRaises(TypeError):
+            max_integer([6, 5, 6], [5])
+
+if __name__ == '__main__':
+    unittest.main()
