a2de6bc181736bb36390471691493cb7082f0a2b
Yoseph Endale <endaleyoseph1@gmail.com>
2023-01-09 08:19:03 -0800

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 10-sqaure
+Defines class square
+with initilization
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Inherites from class rectangle
+    Functions:
+      __init__
+    """
+    def __init__(self, size):
+        """
+        Initilizes square
+        Args:
+           size (int): private instance
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        returns the area of sqaure
+        """
+        return self.__size * self.__size
