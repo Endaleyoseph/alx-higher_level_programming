501c450f0faca6e694ff5c6fdcb380fe144b25df
Yoseph Endale <endaleyoseph1@gmail.com>
2023-01-09 06:51:25 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Modlue 6-base_geometry
+Defines class BaseGeomtry
+with public instance method
+"""
+
+
+class BaseGeometry:
+    """
+    Functions:
+     area
+    """
+    def area(self):
+        """
+        computes the area of the geometry
+        """
+        raise Exception("area() is not implemented")
