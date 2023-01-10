d8af40f3b49b97091e0c6a582c0cc073003e6344
Yoseph Endale <endaleyoseph1@gmail.com>
2023-01-10 06:18:53 -0800

    JSON to string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 3-to_json_string
+Define to_json_string
+JSON representation of object
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns JSON string representation"""
+    return json.dumps(my_obj)
